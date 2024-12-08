6a8e16bfbd7b55b9e755b767599f7cbf7eccd305
TomekWojdyla <tw.kota@gmail.com>
2024-12-08 18:18:18 +0100

    Stwory

---
+++ Creature.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+internal class Creature
+{
+    //Właściwości automatyczne + setter/setter
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    //Konstruktor z parametrami
+    public Creature(string name,int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    //Konstruktor bez parametrów
+    public Creature()
+    {
+        //Nic nie wykonuje
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}");
+    }
+
+    public string Info
+    {
+        get { return $"{Name} [{Level}]"; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,5 +5,12 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!");
+
+        //Test Pkt.2 
+        //Creature a = new("Shrek", 8);
+        //a.SayHi();
+        //Console.WriteLine(a.Info);
+        //Animals z = new() { Description = "Rats" };
+        //Console.WriteLine(z.Info);
     }
 }
